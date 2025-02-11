b9c0d91fab21dbe45363c9238be1a47275007c8a
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-02-11 22:39:34 +0600

    1

--- codeforces/liveContests/div2.cpp
+++ codeforces/liveContests/div2.cpp
@@ -10,8 +10,39 @@ int main()
     cin >> t;
     while (t--)
     {
-       int n,k;
-       cin>>n>>k;
+       int n;
+       cin>>n;
+       vector<int> a(n);
+       vector<int> count(n+1, 0);
+       for(int i = 0; i < n; i++){
+        cin>>a[i];
+        count[a[i]]++;
+       }
+       int flag = 1;
+       for(int i = 0; i < n; i++){
+        if(count[i]==1){
+            flag = 0;
+            break;
+        }else if(count[i]  != 0){
+            int rem = count[i];
+            count[i] = 2;
+            count[i + 1] += rem - 2;
+        }
+       }
+       if(!flag){
+        cout<<"NO"<<endl;
+       }else{
+            for(int i = 0; i <= n; i++){
+                if(count[i] % 2 != 0){
+                    cout<<"NO"<<endl;
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag){
+                cout<<"YES"<<endl;
+            }
+       }
        
     }
     return 0;
