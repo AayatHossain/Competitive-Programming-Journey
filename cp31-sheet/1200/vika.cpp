6936e0d613b81988b7614e39c6bd5931a5982dbb
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-02-11 15:18:38 +0600

    Update vika.cpp

--- cp31-sheet/1200/vika.cpp
+++ cp31-sheet/1200/vika.cpp
@@ -7,7 +7,22 @@ int main(){
     long long t;
     cin>>t;
     while(t--){
-        long long n,x;
+        long long n,k;
+        vector<int> a(n);
+        unordered_map<int, vector<int>> m;
+        for(int i = 0; i < n; i++){
+            cin>>a[i];
+            m[a[i]].push_back(i);
+        }
+        for(auto x: m){
+            int key = x.first;
+            vector<int> val = x.second;
+            cout<<key<<end;
+            for(auto y: val){
+                cout<<y<<" ";
+            }
+            cout<<endl;
+        }
         
     }
     return 0;
