394896694cd8e53f900604dc815476231eb979bf
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-02-11 12:38:35 +0600

    1

--- codeforces/liveContests/div4.cpp
+++ codeforces/liveContests/div4.cpp
@@ -1,15 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;asd
-        cout<<n<<endl;
+
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, m;
+        cin >> n >> m;
+        long long fixed = 0;
+        vector<pair<long long,long long>> a(n);
+        for (long long i = 0; i < n; i++)
+        {
+            long long cont = 0;
+            long long score = 0;
+            long long sum = 0;
+            for(long long j = 0; j < m; j++){
+                long long x;
+                cin>>x;
+                sum += x;
+                cont += x;
+                score += cont;
+            }
+            a[i] = make_pair(sum, score);
+            // cout<<sum<<endl;
+            fixed += score;
+        }
+        sort(a.begin(), a.end(), [](const pair<long long, long long> &p1, const pair<long long, long long> &p2) {
+            return p1.first > p2.first;
+        });
+        
+        // for(auto x: a){
+        //     cout<<x.first<<endl;
+        // }
+
+        long long extra = 0;
+        for(long long i = 0; i < n; i++){
+            pair<long long,long long> x = a[i];
+            extra = extra + (n-1-i)*x.first;
+
+        }
+        long long ans = fixed + (m*extra);
+        cout<<ans<<endl;
     }
     return 0;
 }
