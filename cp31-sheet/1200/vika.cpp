a2b37f17907d8d2516cc48a00f64fc5cbd0a6957
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-02-11 15:14:49 +0600

    Create vika.cpp

---
+++ cp31-sheet/1200/vika.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include <numeric>
+using namespace std;
+
+int main(){
+    
+    long long t;
+    cin>>t;
+    while(t--){
+        long long n,x;
+        
+    }
+    return 0;
+}
