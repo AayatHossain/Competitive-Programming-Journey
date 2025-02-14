29039e2e653807de1c83a5c2954f383cf49963f0
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-02-14 12:00:58 +0600

    Update dora.cpp

--- cp31-sheet/1200/dora.cpp
+++ cp31-sheet/1200/dora.cpp
@@ -18,13 +18,33 @@ int main(){
         int max = n;
         int min = 1;
         while(start < end){
-            int maxI = bS(start, end, b);
-            int minI = bS(start, end, b);
+            int maxI = bS(start, end, b,max);
+            int minI = bS(start, end, b, min);
+            int startChanged = 0;
+            int endChanged = 0;
             if(maxI==start){
                 start++;
+                startChanged = 1;
+                max--;
             }
             if(maxI==end){
                 end--;
+                endChanged = 1;
+                max--;
+            }
+            if(minI==start){
+                start++;
+                startChanged = 1;
+                min++;
+            }
+            if(minI==end){
+                end--;
+                endChanged = 1;
+                min++;
+            }
+            if(startChanged==0 && endChanged==0){
+                cout<<min<<" "<<max<<endl;
+                break;
             }
         }
     }
