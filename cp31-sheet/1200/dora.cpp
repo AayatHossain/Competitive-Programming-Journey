000ef7bd3e25f456fcc1992c97d29c0cb709ed45
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-02-14 12:29:15 +0600

    update

--- cp31-sheet/1200/dora.cpp
+++ cp31-sheet/1200/dora.cpp
@@ -1,5 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int bS(int s, int e, vector<pair<int,int>> b, int v){
+    int mid;
+    while(s <= e){
+        mid = (s+e)/2;
+        if(b[mid].first == v){
+            return b[mid].second;
+        }else if(b[mid].first < v){
+            s = mid + 1;
+        }else{
+            e = mid - 1;
+        }
+    }
+    return - 1;
+}
+
 int main(){
     int t;
     cin>>t;
@@ -17,35 +33,53 @@ int main(){
         int end = n-1;
         int max = n;
         int min = 1;
+        int flag = 0;
         while(start < end){
+            
             int maxI = bS(start, end, b,max);
             int minI = bS(start, end, b, min);
-            int startChanged = 0;
-            int endChanged = 0;
-            if(maxI==start){
-                start++;
-                startChanged = 1;
-                max--;
-            }
-            if(maxI==end){
-                end--;
-                endChanged = 1;
-                max--;
-            }
-            if(minI==start){
-                start++;
-                startChanged = 1;
-                min++;
-            }
-            if(minI==end){
-                end--;
-                endChanged = 1;
-                min++;
-            }
-            if(startChanged==0 && endChanged==0){
-                cout<<min<<" "<<max<<endl;
+            
+            // if(max==6 && min==5){
+            //     cout<<maxI<<" "<<minI<<" ";
+            //     cout<<endl;
+            // }
+            if(maxI==-1 || minI== -1){
                 break;
             }
+
+
+            int startChanged = 0;
+            int endChanged = 0;
+            if(maxI==start && startChanged==0){
+                start++;
+                startChanged = 1;
+                max--;
+            }
+            if(maxI==end && endChanged==0){
+                end--;
+                endChanged = 1;
+                max--;
+            }
+            if(minI==start && startChanged==0){
+                start++;
+                startChanged = 1;
+                min++;
+            }
+            if(minI==end && endChanged==0){
+                end--;
+                endChanged = 1;
+                min++;
+            }
+            // cout<<start<<" "<<end<<"with max and min = "<<max<<" "<<min<<endl;
+            if(startChanged==0 && endChanged==0){
+                cout<<min<<" "<<max<<endl;
+                flag = 1;
+                break;
+            }
+            
+        }
+        if(!flag){
+            cout<<-1<<endl;
         }
     }
     return 0;
