ce66c4f80b3a1f157f90aefe461dc41b49e34c94
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-04-16 15:47:59 +0600

    cf

--- codeforces/ratedProblems/1000/infinite.cpp
+++ codeforces/ratedProblems/1000/infinite.cpp
@@ -3,21 +3,50 @@ using namespace std;
 signed main(){
     int t;cin>>t;
     while(t--){
-        string s,t;cin>>s>>t;
-        int hasA = 0;
-        for(int i = 0; i < t.size(); i++){
-            if(t[i]=='a'){
-                hasA = 1;
-                break;
+        int n;cin>>n;
+        vector<int> a(n);
+        for(int i = 0; i < n; i++){
+            cin>>a[i];
+        }
+        string s;cin>>s;
+        // cout<<s;
+
+        int m = s.size();
+        vector<pair<int,int>> ans;
+        
+        
+        vector<pair<int,int>> dl;
+        vector<pair<int,int>> l;
+        for(int i = 0; i < n; i++){
+            if(s[i]=='0'){
+                dl.push_back({a[i], i});
+               
             }
         }
-        if(hasA && t.size() > 1){
-            cout<<-1<<endl;
-        }else if(hasA && t.size() == 1){
-            cout<<1<<endl;
-        }else{
-            cout<<(long long) pow(2, s.size())<<endl;
+      
+        for(int i = 0; i < m; i++){
+            if(s[i]=='1'){
+                l.push_back({a[i], i});
+            }
         }
+        // cout<<ans.size()<<endl;
+        sort(l.begin(), l.end());
+        sort(dl.begin(), dl.end());
+        int count = 1;
+        for(int i = 0; i < dl.size(); i++){
+            ans.push_back({dl[i].second, count});
+            count++;
+        }
+        for(int i = 0; i < l.size(); i++){
+            ans.push_back({l[i].second, count});
+            count++;
+        }
+        sort(ans.begin(), ans.end());
+        for(int i = 0; i<n; i++){
+            cout<<ans[i].second<<" ";
+        }
+        cout<<endl;
+
     }
     return 0;
 }
