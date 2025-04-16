ad10cf473fb1010cd77e98875ce672a572ce44b6
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-04-16 14:54:40 +0600

    1

---
+++ codeforces/ratedProblems/1000/infinite.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+signed main(){
+    int t;cin>>t;
+    while(t--){
+        string s,t;cin>>s>>t;
+        int hasA = 0;
+        for(int i = 0; i < t.size(); i++){
+            if(t[i]=='a'){
+                hasA = 1;
+                break;
+            }
+        }
+        if(hasA && t.size() > 1){
+            cout<<-1<<endl;
+        }else if(hasA && t.size() == 1){
+            cout<<1<<endl;
+        }else{
+            cout<<pow(2, s.size())<<endl;
+        }
+    }
+    return 0;
+}
