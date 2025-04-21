dbacb1958ad2306e78063c4db74d05d8dfd500d6
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-04-21 15:21:26 +0600

    Create u.cpp

---
+++ Mentor guide/longContest3/u.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+vector<int> g[(int)1e5 + 1];
+using namespace std;
+int main(){
+    int t;cin>>t;
+    int it = 1;
+    while(t--){
+        int n;cin>>n;
+        for(int i = 0; i < n-1; i++){
+            int s,d;cin>>s>>d;
+
+        }
+    }
+}
