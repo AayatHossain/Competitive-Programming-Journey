4dcdb27c117bc044f210189af2eefd2b7b36fad9
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-04-23 12:02:08 +0600

    cpp

--- Mentor guide/longContest3/ac.cpp
+++ Mentor guide/longContest3/ac.cpp
@@ -1,65 +1,77 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define int long long
 // class Pos{   works the same way
 //     public:
 //     int node; int isUsed; int cost;
 // };
-struct Pos{
-    int node; int isUsed; int cost;
+struct Pos
+{
+    int node;
+    int isUsed;
+    int cost;
 };
-auto cmp = [](const Pos &a, const Pos &b){
-    return a.cost < b.cost;
+auto cmp = [](const Pos &a, const Pos &b)
+{
+    return a.cost > b.cost;
 };
-vector<vector<pair<int,int>>> g;
+vector<vector<pair<int, int>>> g;
 vector<vector<int>> dist;
 priority_queue<Pos, vector<Pos>, decltype(cmp)> q(cmp);
-int n,m;
-int dijkstra(){
+int n, m;
+int dijkstra()
+{
     q.push({1, 0, 0});
     dist[1][0] = 0;
-    while(!q.empty()){
+    dist[1][1] = 0;
+    while (!q.empty())
+    {
         Pos p = q.top();
         q.pop();
         int u = p.node;
         int used = p.isUsed;
         int c = p.cost;
-
-        if(c != dist[u][used]){
+       
+        if (c != dist[u][used])
+        {
             continue;
         }
+        // if(u==n){
+        //     break;
+        // }
         
-        if(used){
-            for(auto x: g[u]){
-                int v = x.first;
-                int wt = x.second;
-                if(c + wt < dist[v][0]){
-                    dist[v][0] = c + wt;
-                    q.push({v, 0, dist[v][0]});
-                }
-
-            }
-        }else{
-            for(auto x: g[u]){
-                int v = x.first;
-                int wt = x.second;
-                if(c + wt < dist[v][1]){
-                    dist[v][1] = c + wt/2;
+        for (auto x : g[u])
+        {
+            int v = x.first;
+            int wt = x.second;
+            if(!used){
+                if (c + wt < dist[v][1])
+                {
+                    dist[v][1] = c + wt / 2;
                     q.push({v, 1, dist[v][1]});
                 }
+            }
 
+            if (c + wt < dist[v][used])
+            {
+                dist[v][used] = c + wt;
+                q.push({v, used, dist[v][used]});
             }
         }
     }
     return dist[n][1];
 }
-int main(){
-    cin>>n>>m;
-    g.resize(n+1);
-    dist.resize(n+1, vector<int>(2, INT_MAX));
-    for(int i = 1; i <= n; i++){
-        int s,d,w; cin>>s>>d>>w;
-        g[s].push_back({d,w});
+signed main()
+{
+    cin >> n >> m;
+    g.resize(n + 1);
+    dist.resize(n + 1, vector<int>(2, INT_MAX));
+    for (int i = 1; i <= m; i++)
+    {
+        int s, d, w;
+        cin >> s >> d >> w;
+        g[s].push_back({d, w});
     }
-    cout<<dijkstra()<<endl;
+    cout << dijkstra() << endl;
     return 0;
 }
