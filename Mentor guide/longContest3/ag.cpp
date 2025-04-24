fa8077fc062f45cf4d5ca5805282d48be07bda3e
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-04-24 15:01:39 +0600

    Update ag.cpp

--- Mentor guide/longContest3/ag.cpp
+++ Mentor guide/longContest3/ag.cpp
@@ -14,19 +14,18 @@ signed main(){
     } 
     vector<int> par(n,-1);
     vector<int> dist(n, 0);
-    dist[0] = 0;
     int x = -1;
     for(int i = 0; i < n ; i++){
        
         x = -1;
         for(auto edge: edges){
-            if(dist[edge.u] < LLONG_MAX){
+            
                 if(dist[edge.v] > dist[edge.u] + edge.w){
                     dist[edge.v] = max(LLONG_MIN, dist[edge.u] + edge.w);
                     par[edge.v] = edge.u;
                     x = edge.v;
                 }
-            }
+            
         }
         
     }
