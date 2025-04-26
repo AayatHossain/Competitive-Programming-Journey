bf5e9f92d435b92814e0049495de18dd25217686
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-04-26 15:37:19 +0600

    c

--- Mentor guide/longContest3/live.cpp
+++ Mentor guide/longContest3/live.cpp
@@ -7,28 +7,12 @@ signed main()
     cin >> t;
     while (t--)
     {
+        int n,a,b;cin>>n>>a>>b;
         string s; cin>>s;
-        vector<int> a;
-        for(int i = 0; i < s.size(); i++){
-            a.push_back(s[i] - '0');
-        }
-        sort(a.begin(), a.end());
-        // for(auto x: a){
-        //     cout<<x<<" ";
-        // }
-        int count = 9;
-        string ans;
-        for(int i = count; i >= 0; i--){
-            for(int j = 0; j < a.size(); j++){
-                if(a[j] >= i && a[j] != -1){
-                    ans+=to_string(a[j]);
-                    // cout<<a[j]<<" ";
-                    a[j] = -1;
-                    break;
-                }
-            }
-        }
-        cout<<ans<<endl;
+        int blocks = unique(s.begin(), s.end()) - s.begin();
+        int ans1 = a*n + b*n;
+        int ans2 = a*n + b*(blocks/2 + 1);
+        cout<<max(ans1, ans2)<<endl;
             
     }
 }
