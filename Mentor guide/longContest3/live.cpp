31081ad125f24b35257898a6103bfccb62e2dce7
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-04-27 10:29:32 +0600

    v

--- Mentor guide/longContest3/live.cpp
+++ Mentor guide/longContest3/live.cpp
@@ -7,12 +7,23 @@ signed main()
     cin >> t;
     while (t--)
     {
-        int n,a,b;cin>>n>>a>>b;
-        string s; cin>>s;
-        int blocks = unique(s.begin(), s.end()) - s.begin();
-        int ans1 = a*n + b*n;
-        int ans2 = a*n + b*(blocks/2 + 1);
-        cout<<max(ans1, ans2)<<endl;
+        int n; cin>>n;
+        int c0 = 0, c1 = 0, c2 = 0;
+        for(int i = 0; i < n; i++){
+            int x; cin>>x;
+            if(x % 3 == 0){c0++;}
+            if(x % 3 == 1){c1++;}
+            if(x % 3 == 2){c2++;}
+        }
+        int curr = 0;
+        int moves = 0;
+        while(c0 != c1 || c1 != c2){
+            if(curr==0){ if(c0 > n/3) {moves += (c0 - n/3); c1+= (c0 - n/3); c0 -= (c0 - n/3);  }curr = 1; }
+            if(curr==1){ if(c1 > n/3) {moves += (c1 - n/3); c2+= (c1 - n/3); c1 -= (c1 - n/3); } curr = 2;}
+            if(curr==2){ if(c2 > n/3) {moves += (c2 - n/3); c0+= (c2 - n/3); c2 -= (c2 - n/3); } curr = 0;}
+            // cout<<c0<<" "<<c1<<" "<<c2<<endl;
+        }
+        cout<<moves<<endl;
             
     }
 }
