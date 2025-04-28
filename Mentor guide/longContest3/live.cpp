5cf9fef0dcca528f97a9760ef4a30713891da0f8
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-04-28 09:56:50 +0600

    v

--- Mentor guide/longContest3/live.cpp
+++ Mentor guide/longContest3/live.cpp
@@ -7,7 +7,44 @@ signed main()
     cin >> t;
     while (t--)
     {
-        int n, char b; string s;cin>>n>>b>>s;
+        int n; char c; string s;cin>>n>>c>>s;
+        char last, secLast;
+        secLast = s[n-2];
+        last = s[n-1];
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(s[i] != c){count++;}
+        }
+        if(count==0){
+            cout<<0<<endl;
+        }else{
+            vector<int> ans;
+            bool ok = false;
+            for(int i = 2; i <= n; i++){
+                ok = false;
+                for(int j = i; j <= n; j+=i ){
+                    if(s[j-1] == c){
+                        ok = true;
+                    }else{
+                        ok = false;
+                        break;
+                    }
+                }
+                if(ok){
+                    ans.push_back(i);
+                    break;
+                }
+            }
+            if(!ok){
+                ans.push_back(n-1);
+                ans.push_back(n);
+            }
+            cout<<ans.size()<<endl;
+            for(auto x: ans){
+                cout<<x<<" ";
+            }
+            cout<<endl;
+        }
 
     }
 }
