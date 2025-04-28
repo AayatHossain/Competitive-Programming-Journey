c9dcb863efc416027b0de7be114f9cf84b799a78
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-04-28 18:03:44 +0600

    v

--- Mentor guide/longContest3/live.cpp
+++ Mentor guide/longContest3/live.cpp
@@ -1,78 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-bool palindrome(string s)
-{
-    int i = 0, j = s.size() - 1;
-    while (i < j)
-    {
-        if (s[i] != s[j])
-        {
-            return false;
-        }
-        else
-        {
-            i++;
-            j--;
-        }
-    }
-    return true;
-}
+
 signed main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        int n;
-        cin >> n;
-        string s;
-        cin >> s;
-        int ans = LLONG_MAX;
-        if (palindrome(s))
-        {
-            cout << 0 << endl;
-            continue;
-        }
-        for (char c = 'a'; c <= 'z'; c++)
-        {
-            int count = 0;
-            int i = 0, j = s.size() - 1;
-            while (i < j)
-            {
-                if (s[i] == s[j])
-                {
-                    i++;
-                    j--;
-                }
-                else if (s[i] == c && s[j] != c)
-                {
-                    i++;
-                    count++;
-                }
-                else if (s[i] != c && s[j] == c)
-                {
-                    j--;
-                    count++;
-                }
-                else
-                {
-                    count = LLONG_MAX;
-                    break;
-                }
+        int n;cin>>n;
+        string s; cin>>s;
+        int z = 0;
+        for(int i = 0; i < n; i++){
+            if(s[i]=='0'){
+                z++;
             }
-            //    if(count==3){
-            //     cout<<c<<endl;
-            //    }
-            ans = min(ans, count);
         }
-        if (ans == LLONG_MAX)
-        {
-            cout << -1 << endl;
-        }
-        else
-        {
-            cout << ans << endl;
+        if(z==1){
+            cout<<"BOB"<<endl;
+        }else if(z % 4 == 0){
+            cout<<"DRAW"<<endl;
+        }else if(z % 2 == 0){
+            cout<<"BOB"<<endl;
+        }else{
+            cout<<"ALICE"<<endl;
         }
     }
 }
