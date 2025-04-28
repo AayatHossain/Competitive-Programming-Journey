bd388fd568353e352308be67365665330cb5d9ae
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-04-28 11:19:44 +0600

    V

--- Mentor guide/longContest3/live.cpp
+++ Mentor guide/longContest3/live.cpp
@@ -1,13 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-bool palindrome(string s){
+bool palindrome(string s)
+{
     int i = 0, j = s.size() - 1;
-    while(i < j){
-        if(s[i]!=s[j]){
+    while (i < j)
+    {
+        if (s[i] != s[j])
+        {
             return false;
-        }else{
-            i++; j--;
+        }
+        else
+        {
+            i++;
+            j--;
         }
     }
     return true;
@@ -18,42 +24,55 @@ signed main()
     cin >> t;
     while (t--)
     {
-        int n;cin>>n;string s;cin>>s;
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
         int ans = LLONG_MAX;
-        if(palindrome(s)){
-            cout<<0<<endl;
+        if (palindrome(s))
+        {
+            cout << 0 << endl;
             continue;
         }
-        for(char c = 'a'; c <= 'z'; c++){
-            string newString;
-            for(int j = 0; j < n; j++){
-                if(s[j] != c){
-                    newString+=s[j];
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            int count = 0;
+            int i = 0, j = s.size() - 1;
+            while (i < j)
+            {
+                if (s[i] == s[j])
+                {
+                    i++;
+                    j--;
+                }
+                else if (s[i] == c && s[j] != c)
+                {
+                    i++;
+                    count++;
+                }
+                else if (s[i] != c && s[j] == c)
+                {
+                    j--;
+                    count++;
+                }
+                else
+                {
+                    count = LLONG_MAX;
+                    break;
                 }
             }
-            if(palindrome(newString)){
-                int count = 0;
-                int i = 0, j = s.size() - 1;
-                while(i < j){
-                    if(s[i]==s[j]){
-                        i++; j--;
-                    }else if(s[i]==c && s[j] != c){
-                        i++; count++;
-                    }else{
-                        j--; count++;
-                    }
-                }
-            //    if(count==1){
+            //    if(count==3){
             //     cout<<c<<endl;
             //    }
-                ans = min(ans, count);
-            }
+            ans = min(ans, count);
         }
-        if(ans==LLONG_MAX){
-            cout<<-1<<endl;
-        }else{
-            cout<<ans<<endl;
+        if (ans == LLONG_MAX)
+        {
+            cout << -1 << endl;
         }
-
+        else
+        {
+            cout << ans << endl;
+        }
     }
 }
