6bbe9931b44704d0f41cd463e41bc6f85344717c
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-04-29 10:28:27 +0600

    v

--- Mentor guide/longContest3/live.cpp
+++ Mentor guide/longContest3/live.cpp
@@ -8,20 +8,26 @@ signed main()
     cin >> t;
     while (t--)
     {
-        int n;cin>>n;
-        string s; cin>>s;
-        int z = 0;
+        int n; cin>>n;
+        vector<int> a(n);
         for(int i = 0; i < n; i++){
-            if(s[i]=='0'){
-                z++;
-            }adasasda
+            cin>>a[i];
         }
-        if(z==1){
-            cout<<"BOB"<<endl;
-        }else if(z % 2 == 0){
-            cout<<"BOB"<<endl;
-        }else{
-            cout<<"ALICE"<<endl;
+        vector<int> b = a;
+        sort(b.rbegin(), b.rend());
+        int j = 0;
+        cout<<b[0];
+        int sum = 0;
+        for(int i = n -1; i >= 1; i--){
+            sum += a[i];
+            if(a[i] == b[j]){
+                j++;
+            }
+            sum += b[j];
+            cout<<" "<<sum;
+            sum -= b[j];
         }
+        cout<<'\n';
+
     }
 }
