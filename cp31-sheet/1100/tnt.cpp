50815e42f12ecb9ebf209c90b0da5355d14c5f3b
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-04-30 09:01:00 +0600

    v

---
+++ cp31-sheet/1100/tnt.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int k, x, y; cin>>k>>x>>y;
+        int n1 = (x+y)/2;
+        int ans = (x+n1)/2;
+        cout<<ans<<endl;
+        
+    }
+    return 0;
+}
