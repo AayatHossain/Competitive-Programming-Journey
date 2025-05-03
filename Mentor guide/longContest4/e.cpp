9aee3b8183e471f76fc31aeca840278e4b2f1447
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-05-03 23:43:08 +0600

    Update e.cpp

--- Mentor guide/longContest4/e.cpp
+++ Mentor guide/longContest4/e.cpp
@@ -2,44 +2,67 @@
 using namespace std;
 #define int long long
 const int N = 1e5+7;
-vector<int> a(N);
-vector<int> st(4*N);
+struct node{
+    int val=0;
+    int a=0;
+    int b=0;
+    int c=0;
+    int val(int i){
+        return a*i*i + b*i + c;
+    }
+    node(int v){
+        val = v;
+    }
+    node(){
+        a=b=c=0;
+    }
+};
+int n,q;
+vector<int>a(N);
+vector<node>st(4*N);
 void build(int i, int l, int r){
     if(l==r){
-        st[i] = a[l]; return;
+        st[i] = node(a[l]);
+        return;
     }
-    int mid = l + (r-l)/2;
-    build(2*i+1, l, mid);
-    build(2*i+2, mid + 1, r);
+    int mid = l+(r-l)/2;
+    build(2*i+1,l,mid);
+    build(2*i+2,mid+1,r);
     st[i] = 0;
 }
-void addRange(int i, int l, int r, int s, int e, int val){
-    if(s > e){
+void update(int i, int l, int r, int s, int e, int v, int begL){
+    if(l >e || r<s){
         return;
     }
-    if(s==l && e==r){
-        st[i] += val;
+    if(l>=s && r<=e){
+        st[i].a++;
+        st[i].b += (v-begL);
+        st[i].c += (v-begL)*(v-begL);
         return;
     }
-    int mid = l + (r-l)/2;
-    
+    int mid = l+(r-l)/2;
+    update(2*i+1, l,mid,s,e,v,begL);
+    update(2*i+2, mid+1,r,s,e,v,begL);
+}
+int get(int i, int l,int r,int index){
+    if(l==r && l==index){
+        int sum = st[i].val;
+    }
 }
 signed main(){
-    int n,q; cin>>n>>q;
+    cin>>n>>q;
     for(int i = 0; i < n; i++){
         cin>>a[i];
     }
-    build(0,0,n-1);
+    build();
     for(int i = 0; i < q; i++){
-        int x; cin>>x;
+        int x;cin>>x;
         if(x==1){
-            //update
-            int s,e,v; cin>>s>>e>>v;
-            s--;e--;
-            addRange(0,0,n-1,s,e,v);
+            int l,r,v; cin>>l>>r>>x;
+            update();
         }else{
-            int s; cin>>s; s--;
-            cout<<getVal(0,0,n-1,s)<<endl;
+            int indx;
+            cout<<get()<<endl;
         }
     }
     return 0;
