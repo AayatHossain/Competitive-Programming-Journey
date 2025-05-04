98426e1d02bd8634c62e9ce37a25eab360d426a0
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-05-04 21:58:29 +0600

    s

--- Mentor guide/longContest4/cf1k.cpp
+++ Mentor guide/longContest4/cf1k.cpp
@@ -4,24 +4,11 @@ signed main(){
     int t;cin>>t;
     while(t--){
         int n; cin>>n;
-        vector<int> a(n);
-        for(int i = 0; i < n; i++){
-            cin>>a[i];
+        if((n & (n-1))==0){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
         }
-        deque<int> dq;
-        dq.push_back(a[0]);
-        for(int i = 1; i < n; i++){
-            if(dq.front() > a[i]){
-                dq.push_front(a[i]);
-            }else{
-                dq.push_back(a[i]);
-            }
-        }
-        while(!dq.empty()){
-            cout<<dq.front()<<" ";
-            dq.pop_front();
-        }
-        cout<<endl;
     }
     return 0;
 }
