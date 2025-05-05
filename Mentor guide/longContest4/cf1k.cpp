66ca304aa78584473c59688732a739faf335f15c
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-05-05 07:45:48 +0600

    s

--- Mentor guide/longContest4/cf1k.cpp
+++ Mentor guide/longContest4/cf1k.cpp
@@ -1,10 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool isSqr(int n){
+    int m = sqrt(n);
+    return m*m == n;
+}
 signed main(){
     int t;cin>>t;
     while(t--){
         int n; cin>>n;
-        if((n & (n-1))==0){
+        if((n%2==0 && isSqr(n/2)) || (n%4==0 && isSqr(n/4))){
             cout<<"YES"<<endl;
         }else{
             cout<<"NO"<<endl;
