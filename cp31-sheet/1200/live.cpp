d924534b95b77d98548857af173ffe82a66c99eb
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-05-06 17:58:56 +0600

    Update live.cpp

--- cp31-sheet/1200/live.cpp
+++ cp31-sheet/1200/live.cpp
@@ -1,59 +1,39 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 #define int long long
-
-bool process(vector<int>& arr, int k) {
-    map<int, int> freq;
-
-    // Initialize frequency map
-    for (int x : arr) freq[x]++;
-
-    while (true) {
-        // Skip if empty (shouldn't happen)
-        if (freq.empty()) return true;
-
-        int minVal = freq.begin()->first;
-        int maxVal = freq.rbegin()->first;
-
-        // Base case: difference already within k
-        if (maxVal - minVal <= k) return true;
-
-        bool changed = false;
-
-        // Try to decrease min if > 0
-        if (minVal > 0 && (maxVal - (minVal - 1) <= k)) {
-            int count = freq[minVal];
-            freq[minVal] -= count;
-            if (freq[minVal] == 0) freq.erase(minVal);
-            freq[minVal - 1] += count;
-            changed = true;
-        }
-        // Else try to decrease max
-        else if (maxVal > 0 && ((maxVal - 1) - minVal <= k)) {
-            int count = freq[maxVal];
-            freq[maxVal] -= count;
-            if (freq[maxVal] == 0) freq.erase(maxVal);
-            freq[maxVal - 1] += count;
-            changed = true;
-        }
-
-        // Neither option was possible, exit
-        if (!changed) return false;
-    }
-}
-
-signed main() {
+signed main(){
     int t;cin>>t;
     while(t--){
-        int n, k;
-    cin >> n >> k;
-    vector<int> arr(n);
-    for (int &x : arr) cin >> x;
-
-    bool ok = process(arr, k);
-    cout << (ok ? "YES" : "NO") << endl;
+        int n;cin>>n;
+        vector<int> a(n);
+        for(int i = 0; i < n; i++){
+            cin>>a[i];
+        }
+        vector<int> b = a;
+        sort(b.begin(), b.end());
+        int same = 1;
+        for(int i = 0; i < n-1; i++){
+            if(a[i] != a[i+1]){
+                same = 0;
+                break;
+            }
+        }
+        int mv = b[n-1];
+        // cout<<mv<<endl;
+        if(same){
+            cout<<"NO"<<endl;
+        }else{
+            cout<<"YES"<<endl;
+            int flag = 0;
+            for(int i = 0; i < n; i++){
+                if(!flag && a[i]==mv){
+                    cout<<2<<" ";
+                    flag = 1;
+                }else{
+                    cout<<1<<" ";
+                }
+            }
+            cout<<endl;
+        }
     }
-
-    return 0;
 }
