10966344b67995cbec0f0f23a116ae76b11109d8
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-06-11 19:20:01 +0600

    s
    s

--- Mentor guide/longContest6/.vscode/y.cpp
+++ Mentor guide/longContest6/.vscode/y.cpp
@@ -2,26 +2,24 @@
 using namespace std;
 #define int long long
 const int N = 1e6+7;
-pair<int,int> dp[N];
-int f(int cn, int cs){
-    if(cs==0)return cn;
-    if(cs < 0) return LLONG_MAX/2;
-    if(dp[cs].first != -1 && dp[cs].second <= cn)return dp[cs].second;
-    
-    int v1 = f(cn*10 + 7, cs - 7);
-    int v2 = f(cn*10 + 4, cs - 4);
-    
-    dp[cs].second = min (v1,v2);
-    dp[cs].first = cs;
-    return dp[cs].second;
+int n;
+int dp[N][N];
+int f(int sum, int num){
+    if(sum==n)return num;
+    if(sum > n) return LLONG_MAX/2;
+    if(dp[sum][num] != -1)return dp[sum][num];
+    int v1 = f(sum + 4, num*10 + 4);
+    int v2 = f(sum + 7, num*10 + 7);
+    return dp[sum][num] = min(v1,v2);
 }
 signed main(){
-    memset(dp, -1, sizeof(dp));
-    int n; cin>>n;
-    int ans = f(0,n);
-    if(ans>=LLONG_MAX/2){
+    memset(dp, -1, sizeof dp);
+    cin>>n;
+    int ans = f(0,0);
+    if(ans==LLONG_MAX/2){
         cout<<-1<<endl;
     }else{
-        cout<<ans<<endl;
+    cout<<f(0,0)<<endl;
+
     }
 }
