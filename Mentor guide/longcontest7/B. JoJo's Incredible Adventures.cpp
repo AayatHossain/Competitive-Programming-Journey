f0bfea0f041378ce89203e032f9e109badfc5947
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-07-08 21:05:34 +0600

    s

---
+++ Mentor guide/longcontest7/B. JoJo's Incredible Adventures.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        // string s;
+        // cin >> s;
+        // int n = s.size();
+        // s = s + s;
+        // int mx = 0;
+        // int o = 0;
+        // for (int i = 0; i < 2*n; i++)
+        // {
+        //     if (s[i] == '1')
+        //     {
+        //         o++;
+        //         mx = max(mx, o);
+        //     }
+        //     else
+        //     {
+        //         o = 0;
+        //     }
+        // }
+
+        // if(mx==2*n){
+        //     // cout<<"i";
+        //     mx = n;
+        // }
+        // // cout<<mx<<endl;
+
+        // int ans;
+        // if (mx == n)
+        // {
+        //     ans = n * n;
+        //     cout << ans << endl;
+        //     continue;
+        // }
+
+        // if (mx == 0)
+        // {
+        //     ans = 0;
+        // }
+        // else if (mx == 1)
+        // {
+        //     ans = 1;
+        // }
+        // else
+        // {
+        //     int r = ceil(mx / 2.0);
+        //     int c = mx / 2 + 1;
+        //     // cout<<r<<c<<endl;
+        //     ans = r * c;
+        // }
+
+        // cout << ans << endl;
+
+        cout<<(6+1)/2<<endl;
+        cout<<(6+2)/2<<endl;
+    }
+    return 0;
+}
