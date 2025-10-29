6c13bcb8a85b06f7d7a74583762db48ea87e85e2
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2025-10-29 21:11:32 +0600

    s

--- ultimateTopicList/graph/shortest path/dijkstra/Flight Routes.cpp
+++ ultimateTopicList/graph/shortest path/dijkstra/Flight Routes.cpp
@@ -1,46 +1,55 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-
-const int MX = 2e5 + 5;
-int n, m, k;
-priority_queue<ll> bes[MX];
-vector<pair<int, int>> adj[MX];
-priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
-
-int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-
-	cin >> n >> m >> k;
-	for (int i = 0; i < m; i++) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		adj[a].push_back({b, c});
-	}
-	bes[1].push(0);
-	pq.push({0, 1});
-	while (!pq.empty()) {
-		auto a = pq.top();
-		pq.pop();
-		if (a.first > bes[a.second].top()) continue;
-		for (auto &i : adj[a.second]) {
-			ll tmp = a.first + i.second;
-			if (bes[i.first].size() < k) {
-				bes[i.first].push(tmp);
-				pq.push({tmp, i.first});
-			} else if (tmp < bes[i.first].top()) {
-				bes[i.first].pop();
-				bes[i.first].push(tmp);
-				pq.push({tmp, i.first});
-			}
+#define int long long
+int n,m,k;
+const int N = 1e5+1;
+vector<pair<int,int>> g[N];
+// vector<int> d(N,LLONG_MAX/2);
+priority_queue<int> g2[N];
+void f(int s){
+	priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> q;
+	q.push({0,s});
+	g2[s].push(0);
+	// d[s]=0;
+	while(!q.empty()){
+		int u = q.top().second;
+		int du = q.top().first;
+		q.pop();
+		if(du > g2[u].top())continue;
+		for(auto x: g[u]){
+			int v= x.first;
+			int w = x.second;
+			int tmp = du+w;
+			
+				if(g2[v].size() == k && g2[v].top() > tmp){
+					g2[v].pop();
+					g2[v].push({tmp});
+					q.push({tmp, v});
+				}else if(g2[v].size() < k){
+					g2[v].push({tmp});
+					q.push({tmp, v});
+					
+				}
+			
 		}
 	}
-	vector<ll> ans;
-	while (!bes[n].empty()) {
-		ans.push_back(bes[n].top());
-		bes[n].pop();
+}
+signed main(){
+	cin>>n>>m>>k;
+	for(int i = 0; i < m; i++){
+		int u,v,w; cin>>u>>v>>w;
+		g[u].push_back({v,w});
+	}
+	f(1);
+	vector<int> ans;
+	while(!g2[n].empty()){
+		ans.push_back(g2[n].top());
+		g2[n].pop();
 	}
 	reverse(ans.begin(), ans.end());
-	for (auto a : ans) cout << a << " ";
+	for(auto x: ans){
+		cout<<x<<" ";
+	}
+	cout<<'\n';
+	return 0;
 }
--- ultimateTopicList/graph/shortest path/dijkstra/Investigation.cpp
+++ ultimateTopicList/graph/shortest path/dijkstra/Investigation.cpp
@@ -1,57 +1,53 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define int long long
+int mod = 1e9+7;
+int n,m;
+const int N = 1e5+1;
+vector<pair<int,int>> g[N];
+vector<int> d(N, LLONG_MAX/2);
+vector<int> mn(N, LLONG_MAX/2);
+vector<int> mx(N, LLONG_MIN/2);
+vector<int> cnt(N, 0);
+void f(int s){
+	priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> q;
+	d[s]=0; 
+	cnt[s]=1;
+	mn[s]=0;
+	mx[s]=0;
+	q.push({0,s});
+	while(!q.empty()){
+		int u = q.top().second;
+		int du = q.top().first;
+		q.pop();
+		if(du != d[u])continue;
+		for(auto x: g[u]){
+			int v = x.first;
+			int w = x.second;
+			int tmp = du + w;
+			if(tmp < d[v]){
+				d[v]=tmp;
+				q.push({tmp, v});
+				cnt[v]=cnt[u];
+				mn[v] = mn[u]+1;
+				mx[v] = mx[u]+1;
 
-const int MX = 2e5 + 5;
-int n, m;
-priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> bes[MX];
-vector<pair<int, int>> adj[MX];
-priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
-
-signed main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-
-	cin >> n >> m;
-	for (int i = 0; i < m; i++) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		adj[a].push_back({b, c});
-	}
-	bes[1].push({0,0});
-	pq.push({0, {1,0}});
-	while (!pq.empty()) {
-		auto a = pq.top();
-		pq.pop();
-		if (a.first > bes[a.second.first].top().first) continue;
-		for (auto &i : adj[a.second.first]) {
-			int tmp = a.first + i.second;
-                if(bes[i.first].empty() || bes[i.first].top().first >tmp){
-                    // while(!bes[i.first].empty() && bes[i.first].top().first >tmp){
-                    // }
-                    if(!bes[i.first].empty()){
-                        bes[i.first].pop();
-                    }
-                    bes[i.first].push({tmp, a.second.second+1});
-				    pq.push({tmp, {i.first, a.second.second+1}});
-                }else if(!bes[i.first].empty() && bes[i.first].top().first ==tmp){
-                    bes[i.first].push({tmp, a.second.second+1});
-				    pq.push({tmp, {i.first, a.second.second+1}});
-                }
-				
-			
+			}else if(tmp==d[v]){
+				cnt[v] = (cnt[v]+cnt[u])%mod;
+				mn[v] = min(mn[v], mn[u]+1);
+				mx[v] = max(mx[v], mx[u]+1);
+			}
 		}
 	}
-	vector<int> ans;
-    int c = 0;
-    int mn = LLONG_MAX/2;
-    int mx = LLONG_MIN/2;
-    int v = bes[n].top().first;
-	while (!bes[n].empty() && bes[n].top().first==v) {
-		mx = max(mx, bes[n].top().second);
-		mn = min(mn, bes[n].top().second);
-        c++;
-		bes[n].pop();
+	
+}
+signed main(){
+	cin>>n>>m;
+	for(int i = 0; i <m; i++){
+		int u,v,w; cin>>u>>v>>w;
+		g[u].push_back({v,w});
 	}
-	cout<<v<<" "<<c<<" "<<mn<<" "<<mx<<endl;
+	f(1);
+	cout<<d[n]<<" "<<cnt[n]<<" "<<mn[n]<<" "<<mx[n]<<endl;
+	return 0;
 }
