27c720c1c74300ebe8ea8426d90a6583bc700202
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-03-27 02:57:53 +0600

    scale todo app

---
+++ c2ladder/1300/C. Disappearing Permutation.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+signed main(){
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<int> p(n+1);
+        for(int i = 1; i <= n; i++)cin>>p[i];
+
+        vector<int> d(n+1);
+        for(int i = 1; i <= n; i++)cin>>d[i];
+
+        vector<int> a(n+1);
+        for(int i = 1; i <= n; i++){
+            a[p[i]] = i;
+        }
+        // cout<<n<<endl;
+        // for(auto x: a){
+        //     cout<<x<<" ";
+        // }
+        // cout<<endl;
+
+        int ans = 0;
+        for(int i = 1; i <= n; i++){
+            int curr = d[i];
+            
+            if(a[curr]==curr){
+                cout<<ans<<" ";
+                continue;
+            }
+
+            int c = 0;
+            while(a[curr] != curr){    
+                c++;
+                int curr2 = a[curr];
+                a[curr] = curr;
+                curr = curr2;
+            }
+
+            // a[curr] = curr;
+            c++;
+
+            ans += c;
+            cout<<ans<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
--- c2ladder/1300/contest2.cpp
+++ c2ladder/1300/contest2.cpp
@@ -1,17 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-signed main()
-{
-    int n; cin>>n;
-    int ans = 0;
-    for(int i = 1; i <= n; i++){
-        int p = -1;
-        if(i%2 == 0)p = 1;
-        int x=  p * (i*i*i);
-        ans += x;
+
+const int MAXN = 1e7 + 5;
+
+vector<bool> isPrime(MAXN, true);
+vector<int> cnt(MAXN, 0);
+
+void sieve(int n) {
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i)
+                isPrime[j] = false;
+        }
     }
-    cout<<ans<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    int max_n = 0;
+    vector<int> tests(t);
+    for (int i = 0; i < t; i++) {
+        cin >> tests[i];
+        max_n = max(max_n, tests[i]);
+    }
+
+    // Step 1: Sieve
+    sieve(max_n);
+
+    // Step 2: Count multiples
+    for (int i = 2; i <= max_n; i++) {
+        for (int j = i + i; j <= max_n; j += i) {
+            int k = j / i;
+            if (isPrime[k]) {
+                cnt[j]++;
+            }
+        }
+    }
+
+    // Step 3: Prefix answers
+    vector<int> ans(max_n + 1, 0);
+
+    for (int i = 2; i <= max_n; i++) {
+        ans[i] = ans[i - 1] + cnt[i];
+        if (isPrime[i]) ans[i]++; // for (1, i)
+    }
+
+    // Output
+    for (int n : tests) {
+        cout << ans[n] << '\n';
+    }
 
     return 0;
 }
