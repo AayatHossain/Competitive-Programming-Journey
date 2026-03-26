1d0cb8413ede6afe07ba3a40ddc02c7579499dd7
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-03-26 15:58:22 +0600

    s

--- c2ladder/1300/contest2.cpp
+++ c2ladder/1300/contest2.cpp
@@ -1,74 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 signed main()
 {
-    int n,x,y,z; cin>>n>>x>>y>>z;
-    vector<int> a(n);
-    vector<int> b(n);
-    
-    for(int i= 0; i < n; i++){
-        cin>>a[i];
+    int n; cin>>n;
+    int ans = 0;
+    for(int i = 1; i <= n; i++){
+        int p = -1;
+        if(i%2 == 0)p = 1;
+        int x=  p * (i*i*i);
+        ans += x;
     }
-    for(int i= 0; i < n; i++){
-        cin>>b[i];
-    }
-
-    set<int> s;
-    priority_queue<pair<int,int>> pq;
-    for(int i = 0; i < n; i++){
-        pq.push({a[i],-i});
-    } 
-
-    for(int i = 0; i < x; i++){
-        int v = -pq.top().second;
-        s.insert(v);
-        // cout<<v<<endl;
-        pq.pop();
-    }
-
-    priority_queue<pair<int,int>> pq2;
-    while(!pq.empty()){
-        int index = -pq.top().second;
-        pq.pop();
-
-       
-        // s.insert(index);
-        pq2.push({b[index], -index});
-    }
-
-
-
-
-    //english
-    for(int i = 0; i < y; i++){
-        int v = -pq2.top().second;
-        s.insert(v);
-        
-        pq2.pop();
-    }
-
-    priority_queue<pair<int,int>> pq3;
-    while(!pq2.empty()){
-        int index = -pq2.top().second;
-        //  cout<<index<<endl;
-        pq2.pop();
-
-        // s.insert(index);
-        pq3.push({b[index]+a[index], -index});
-    }
-
-
-    for(int i = 0; i < z; i++){
-        int v = -pq3.top().second;
-        s.insert(v);
-        pq3.pop();
-    }
-
-    for(auto x: s){ 
-        cout<<x+1<<endl;
-    }
-
-
+    cout<<ans<<endl;
 
     return 0;
 }
